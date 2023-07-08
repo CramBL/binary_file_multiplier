f0d6bd634a8b474799d922ce18b192beafb19df7
CramBL <marcbkonig@gmail.com>
2023-07-08 20:09:54 +0200

    fast enough

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,16 @@ pub struct Cfg {
     #[arg(name = "Input data", required = true)]
     file_in: PathBuf,
 
+    /// Desired file size in MiB (actual file size will be closest multiple rounded down)
+    #[arg(
+        name = "Desired output size",
+        short = 's',
+        long = "size",
+        visible_alias = "size-out",
+        required = true
+    )]
+    size_mib: usize,
+
     /// Output file.
     #[arg(
         name = "Output Data",
@@ -33,6 +43,33 @@ pub struct Cfg {
         required = true
     )]
     file_out: PathBuf,
+}
+
+// One Mebibyte in bytes
+const MI_B: usize = 1024 * 1024;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = Cfg::parse();
+
+    let input_path = config.file_in;
+    let output_path = config.file_out;
+    let desired_byte_size = config.size_mib * MI_B;
+
+    let input_contents = read_binary_file(input_path.to_str().unwrap())?;
+
+    let input_contents_size = input_contents.len();
+
+    let duplication_factor = desired_byte_size / input_contents_size;
+    let mut duplicated_content: Vec<u8> =
+        std::vec::Vec::with_capacity(duplication_factor * input_contents_size);
+
+    for _ in 0..duplication_factor {
+        duplicated_content.extend_from_slice(&input_contents);
+    }
+
+    write_binary_file(output_path.to_str().unwrap(), &duplicated_content)?;
+
+    Ok(())
 }
 
 fn read_binary_file(path: &str) -> io::Result<Vec<u8>> {
@@ -45,28 +82,5 @@ fn read_binary_file(path: &str) -> io::Result<Vec<u8>> {
 fn write_binary_file(path: &str, data: &[u8]) -> io::Result<()> {
     let mut file = File::create(path)?;
     file.write_all(data)?;
-    Ok(())
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config = Cfg::parse();
-
-    let input_path = config.file_in;
-    let output_path = config.file_out;
-
-    let contents = read_binary_file(input_path.to_str().unwrap())?;
-
-    let size = contents.len();
-
-    // How many to make 100 MB?
-    let duplication_factor = 1024 * 1024 * 100 / size;
-    let mut bytes_100_mb: Vec<u8> = std::vec::Vec::with_capacity(1024 * 1024 * 100);
-
-    for _ in 0..duplication_factor {
-        bytes_100_mb.extend_from_slice(&contents);
-    }
-
-    write_binary_file(output_path.to_str().unwrap(), &bytes_100_mb)?;
-
     Ok(())
 }
